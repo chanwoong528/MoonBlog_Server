0da3ada05d956f79ff81cb9a318db5dca2b36834
chanwoong528 <cksdnd004@naver.com>
2022-01-27 00:00:51 +0900

    route for adding posts' topic

---
+++ model/Topic.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const topicSchema = new mongoose.Schema({
+  topic: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  created_at: { type: String, required: true },
+});
+module.exports = mongoose.model("Topic", topicSchema);
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Menu = require("../model/Menu");
+const Topic = require("../model/Topic");
 const router = new express.Router();
 
 router.post("/menu", async (req, res) => {
@@ -19,6 +20,28 @@ router.get("/menu", async (req, res) => {
   console.log(menus);
   if (menus) {
     res.status(200).send({ msg: "Successful to get Menus", menus });
+  }
+});
+
+router.post("/topic", async (req, res) => {
+  const curDate = new Date().toISOString().slice(0, 10);
+  const { topic, description } = req.body;
+  console.log(req.body);
+  console.log(curDate);
+  const newTopic = new Topic({ topic, description, created_at: curDate });
+  await newTopic.save((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ msg: "Unable to create Topic" });
+    } else {
+      return res.status(201).send({ msg: "Topic Created!" });
+    }
+  });
+});
+router.get("/topic", async (req, res) => {
+  const topics = await Topic.find({});
+  if (topics) {
+    res.status(200).send({ msg: "Successful to get topics", topics });
   }
 });
 
